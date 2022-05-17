80effed166426dc6d5cb8fabafc8894d64926732
jaazieljr <jaaziel.dev@gmail.com>
2022-05-17 01:01:51 -0300

    novas alteracoes na interface

---
+++ Iplane-PassagensAereas/src/classes/Aplication.java
@@ -0,0 +1,20 @@
+package classes;
+
+
+import forms.FrmHome;
+
+
+public class Aplication {
+
+  
+    public static void main(String[] args) {
+        
+FrmHome home = new FrmHome();
+home.setLocationRelativeTo(null);
+home.setVisible(true);
+
+
+       
+    }
+    
+}
